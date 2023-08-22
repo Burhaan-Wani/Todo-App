363fdd144cb18eb2ad5544368d56346d6d338b4e
Burhaan <burhaanwani823@gmail.com>
2023-08-23 00:02:38 +0530

    Add listbox and edit btn with their functionality

--- GUI.py
+++ GUI.py
@@ -2,9 +2,14 @@ from get_todos_write_todos import *
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo",key="Todo")
+input_box = sg.InputText(tooltip="Enter todo", key="Todo")
 add_button = sg.Button("Add")
-layout = [[label], [input_box, add_button]]
+list_box = sg.Listbox(values=get_todos(),
+                      key="Todos",
+                      enable_events=True,
+                      size=[40, 10])
+edit_btn = sg.Button("Edit")
+layout = [[label], [input_box, add_button], [list_box, edit_btn]]
 
 window = sg.Window("My To-Do App",
                    layout,
@@ -20,7 +25,18 @@ while True:
             new_todo = values["Todo"] + "\n"
             todo_list.append(new_todo)
             write_todos(todo_list)
+            window["Todos"].update(values=todo_list)
         case sg.WIN_CLOSED:
             break
+        case "Todos":
+            window["Todo"].update(value=values["Todos"][0])
+        case "Edit":
+            todo_to_edit = values["Todos"][0]
+            new_todo = values['Todo']
 
+            todo_list = get_todos()
+            index = todo_list.index(todo_to_edit)
+            todo_list[index] = new_todo
+            write_todos(todo_list)
+            window["Todos"].update(values=todo_list)
 window.close()
