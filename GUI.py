29d739991a613e851e98a10d6379f25bea688017
Burhaan <burhaanwani823@gmail.com>
2023-08-22 23:27:52 +0530

    Implemented Add function to add btn

--- GUI.py
+++ GUI.py
@@ -2,8 +2,25 @@ from get_todos_write_todos import *
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo",key="Todo")
 add_button = sg.Button("Add")
-window = sg.Window("My To-Do App", layout=[[label], [input_box, add_button]])
-window.read()
+layout = [[label], [input_box, add_button]]
+
+window = sg.Window("My To-Do App",
+                   layout,
+                   font=("Helvetica", 12))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todo_list = get_todos()
+            new_todo = values["Todo"] + "\n"
+            todo_list.append(new_todo)
+            write_todos(todo_list)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
